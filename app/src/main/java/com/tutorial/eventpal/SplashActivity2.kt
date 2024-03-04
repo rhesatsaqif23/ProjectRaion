b52f40f2b53033d91a19e6e2eaa4502f66b95436
rhesatsaqif23 <atstsaqif23@gmail.com>
2024-03-04 22:24:15 +0700

    Progress 1

---
+++ app/src/main/java/com/tutorial/eventpal/SplashActivity2.kt
@@ -0,0 +1,12 @@
+package com.tutorial.eventpal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tutorial.firebaseapp.R
+
+class SplashActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash2)
+    }
+}
---
+++ app/src/main/java/com/tutorial/eventpal/SplashActivity4.kt
@@ -0,0 +1,12 @@
+package com.tutorial.eventpal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tutorial.firebaseapp.R
+
+class SplashActivity4 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash4)
+    }
+}
