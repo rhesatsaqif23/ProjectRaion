b0525fad28f5e2791e11356f822c337ac7ee364c
rhesatsaqif23 <atstsaqif23@gmail.com>
2024-03-07 13:53:38 +0700

    Progress 1

--- app/src/main/java/com/tutorial/eventpal/SplashActivity3.kt
+++ app/src/main/java/com/tutorial/eventpal/SplashActivity3.kt
@@ -1,11 +1,14 @@
 package com.tutorial.eventpal
 
+import android.annotation.SuppressLint
+import android.app.ActivityOptions
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.AppCompatButton
 import com.tutorial.firebaseapp.R
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity3 : AppCompatActivity() {
     lateinit var btnContinue: AppCompatButton
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +17,10 @@ class SplashActivity3 : AppCompatActivity() {
 
         btnContinue = findViewById(R.id.btn_continue2)
         btnContinue.setOnClickListener {
-            startActivity(Intent(this, SplashActivity4::class.java))
+            val intent = Intent(this, SplashActivity4::class.java)
+            val options = ActivityOptions.makeCustomAnimation(this, R.anim.slide_in_right, R.anim.slide_out_left)
+            startActivity(intent, options.toBundle())
         }
+
     }
 }
