ffca7abf70962e5d7fcd7947bc48d13ddcfe425f
rhesatsaqif23 <atstsaqif23@gmail.com>
2024-03-09 09:32:08 +0700

    Progress 1

---
+++ app/src/main/java/com/tutorial/eventpal/AgentDetailActivity1.kt
@@ -0,0 +1,12 @@
+package com.tutorial.eventpal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tutorial.firebaseapp.R
+
+class AgentDetailActivity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agent_detail1)
+    }
+}
