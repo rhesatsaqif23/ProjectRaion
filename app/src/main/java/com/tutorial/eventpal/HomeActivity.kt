ed02a27d097198abdc26cc51f013097aba8c6413
Althaf Rizal <ilikemud7@gmail.com>
2024-03-14 21:23:45 +0700

    sdh berusaha

--- app/src/main/java/com/tutorial/eventpal/HomeActivity.kt
+++ app/src/main/java/com/tutorial/eventpal/HomeActivity.kt
@@ -1,7 +1,26 @@
 package com.tutorial.eventpal
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ReportFragment.Companion.reportFragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.tutorial.firebaseapp.R
 
 class HomeActivity: AppCompatActivity() {
-//    lateinit var homeAdress:
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var searchView: RecyclerView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        recyclerView = findViewById(R.id.recyclerView)
+        searchView = findViewById(R.id.searchView)
+
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        addDataToList()
+    }
 }
