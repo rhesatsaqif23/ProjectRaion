4dc1ee9feb97b2f75921510239f46604d36c5908
rhesatsaqif23 <atstsaqif23@gmail.com>
2024-03-21 20:49:56 +0700

    Progress 1

--- app/src/main/java/com/tutorial/eventpal/LapakActivity1.kt
+++ app/src/main/java/com/tutorial/eventpal/LapakActivity1.kt
@@ -1,12 +1,21 @@
 package com.tutorial.eventpal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
 import com.tutorial.firebaseapp.R
 
 class LapakActivity1 : AppCompatActivity() {
+    lateinit var btnRent: AppCompatButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lapak1)
+
+        btnRent = findViewById(R.id.btn_rent)
+        btnRent.setOnClickListener() {
+            startActivity(Intent(this, LapakConfirmationActivity::class.java))
+            finish()
+        }
     }
 }
